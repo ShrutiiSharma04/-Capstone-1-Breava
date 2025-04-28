d429d8bc1f7106c0b608c4d46f83c1e5180007c9
SambhavJain26 <worksambhav26@gmail.com>
2025-04-28 22:17:24 +0530

    added gitignore to remove venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\apani\Desktop\Breava\venv
+set VIRTUAL_ENV=C:\Web Development\Projects\Capstone1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
